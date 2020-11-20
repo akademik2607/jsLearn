26f41d8e3713aa5967f7e60066193acdb619f4e4
akademik2607 <akademik2607@mail.ru>
2020-11-20 09:18:53 +0300

    Задание ко 2 уроку

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-let mony;
-let incomy;
-let addExpenses;
-let deposit;
-let mission;
-let period;
-alert("Работа alert");
-console.log("Вывод в консоль");
+let mony = 30000;
+let incomy = 'Фриланс';
+let addExpenses = 'интернет, такси, коммуналка';
+let deposit = false;
+let mission = 4000000;
+let period = 6;
+
+console.log(typeof mony);
+console.log(typeof incomy);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log("Период равен " + period + " месяцев.");
+console.log('Цель: заработать', mission, 'рублей');
+console.log(addExpenses.toLowerCase().split(' '));
+
+let budgetDay = mony / 30;
+console.log(budgetDay);
