3adf8052beb9bdebb41daf63b942f94e073fb8a9
akademik2607 <akademik2607@mail.ru>
2020-11-24 07:06:09 +0300

    Выполнил задание к 3 уроку

--- script.js
+++ script.js
@@ -15,3 +15,29 @@ console.log(addExpenses.toLowerCase().split(' '));
 
 let budgetDay = mony / 30;
 console.log(budgetDay);
+
+money = +(prompt('Ваш месячный доход?'));
+addExpenses = prompt("Перечислите ваши дополнительные расходы в месяц через запятую: ");
+deposit = confirm('Есть ли у вас депозит в банке?');
+let expenses1 = prompt("Введите обязательную статью расходов: ");
+let amount1 = +prompt('Во сколько это обойдется?');
+let expenses2 = prompt("Введите обязательную статью расходов: ");
+let amount2 = +prompt('Во сколько это обойдется?');
+
+let budgetMonth = mony / (amount1 + amount2);
+
+console.log('Ваша цель будет достигнута за: ', Math.ceil(mission / budgetMonth), 'месяцев.');
+budgetDay = budgetMonth / 30;
+
+if(budgetDay >= 1200){
+    console.log('У вас высокий уровень дохода!');
+}
+else if(budgetDay >= 600 && budgetDay < 1200){
+    console.log('У вас средний уровень дохода!');
+}
+else if(budgetDay >= 0 && budgetDay < 600){
+    console.log('К сожалению у вас очень низкий уровень дохода(');
+}
+else{
+    console.log('Что-то пошло не так!');
+}
