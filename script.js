101ab7b962f0d6f127ac810d36dca86de747ca7b
akademik2607 <akademik2607@mail.ru>
2020-12-01 22:08:49 +0300

    Начало изменений

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+'use strict'
+
+const isNumber = function(num){
+    return !(isNaN(parseFloat(num))) && isFinite(num);
+}
+
+let money,
+    addExpenses,
+    deposit = false,
+    expenses = [];
+const income = 'Фриланс',
+    mission = 4000000,
+    period = 6,
+    expensesCount = 2;
+
+const start = function (){
+    do{
+    money = (prompt('Ваш месячный доход?'));
+    } while(!isNumber(money)); 
+    money = +money;
+    addExpenses = prompt("Перечислите ваши дополнительные расходы в месяц через запятую: ").toLowerCase().split(', ');
+    console.log(addExpenses);
+    deposit = confirm('Есть ли у вас депозит в банке?');
+};
+
+const showTypeOf = function(item){
+    console.log(typeof item);
+};
+
+
+
+const getExpensesMonth = function(expenses){
+    let sum = 0, temp;
+    for(let i = 0; i < expensesCount; ++i){
+        expenses[i] = prompt("Введите обязательную статью расходов: ");
+        do{
+        temp = prompt("Во сколько это обойдется: ");
+        }while(!isNumber(temp));
+        sum += parseFloat(temp);
+    }
+    console.log(expenses);
+    return sum;
+};
+
+
+const getStatusIncome = function(budgetDay){
+    if(budgetDay >= 1200){
+        console.log('У вас высокий уровень дохода!');
+    }
+    else if(budgetDay >= 600 && budgetDay < 1200){
 'use strict';
 
 
